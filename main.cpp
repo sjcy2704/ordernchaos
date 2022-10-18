320e582a479d19607ad00cff5841ea9299923e08
sjcy2704 <a1834160@adelaide.edu.au>
2022-10-18 23:47:57 +1030

    Fix file error

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include "src/Header Files/Game.h"
 
 int main() {
+  if (!std::__fs::filesystem::exists("build/")) {
+    system("mkdir build");
+  }
+
   Game OrderNChaos;
 
   OrderNChaos.run();
